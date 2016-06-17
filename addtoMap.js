a7ad482944cfef9e2082d31c63f2ecdf018b9f81
Chris Monk <cfmonk@gmail.com>
2016-06-17 16:04:08 +1000

    Serve icon over http

--- addtoMap.js
+++ addtoMap.js
@@ -2,7 +2,7 @@ function drawpoints(data){
 
 	//Define an icon for our markers to overwrite the default
   	var icon = {
-    	url: "https://www.clker.com/cliparts/Y/y/f/f/p/U/atm-machine-sign-hi.png",
+    	url: "http://www.clker.com/cliparts/Y/y/f/f/p/U/atm-machine-sign-hi.png",
         scaledSize: new google.maps.Size(30, 30), // scaled size
         origin: new google.maps.Point(0,0), // origin
         anchor: new google.maps.Point(0, 0), // anchor
